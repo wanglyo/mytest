517bef6ddca882f18ad0fb805acde79535447274
liuyy <liuyy@139.com>
2025-07-23 11:43:11 +0800

    test

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -43,7 +43,7 @@ public class CounterController {
    */
   @GetMapping(value = "/api/count")
   ApiResponse get() {
-    logger.info("/api/count get request");
+    logger.info("/api/count get request 月少18CM");
     Optional<Counter> counter = counterService.getCounter(1);
     Integer count = 0;
     if (counter.isPresent()) {
